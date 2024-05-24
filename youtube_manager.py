a28f6b05bc63bde522d72f08b56184c0d32080ea
Mallepally Lokeshwar Reddy <lokeshwar777@gmail.com>
2024-05-25 01:43:30 +0530

    chore: Refactor youtube_manager.py to use SQLite for data storage

--- youtube_manager.py
+++ youtube_manager.py
@@ -1,58 +1,37 @@
-import json
+import sqlite3
 
-fileName = 'youtube.txt'
+con = sqlite3.connect("youtube_videos.db")
 
-def load_data():
-    try:
-        with open(fileName, 'r') as file:
-            test = json.load(file)
-            print(type(test))
-            return test
-    except FileNotFoundError:
-        return []
+cursor = con.cursor()
 
-def save_data_helper(videos):
-    with open(fileName, 'w') as file:
-        json.dump(videos, file)
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS videos (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            time TEXT NOT NULL
+    )
+''')
 
-def list_all_videos(videos):
+def list_videos():
     print('\n')
     print('*' * 77)
-    for index, video in enumerate(videos, start=1):
-        print(f"{index}. {video['video_name']} {video['video_duration']}")
-    print('\n')
-    print('*' * 77)
+    cursor.execute("SELECT * FROM videos")
+    for row in cursor.fetchall():
+            print(row)
 
-def add_video(videos):
-    video_name = input("Enter video name: ")
-    video_duration = input("Enter the duration: ")
-    videos.append({'video_name': video_name, 'video_duration': video_duration})
-    save_data_helper(videos)
+def add_video(new_video_name, new_video_duration):
+    cursor.execute("INSERT INTO videos (video_name, video_duration) VALUES (?, ?)", (new_video_name, new_video_duration))
+    con.commit()
 
-def update_video(videos):
-    list_all_videos(videos)
-    index = int(input("Enter the video number to update: "))
-    if (1 <= index <= len(videos)):
-        new_video_name = input("Enter the new video name: ")
-        new_video_duration = input("Enter the new video duration: ")
-        videos[index-1] = {'video_name': new_video_name, 'video_duration': new_video_duration}
-        save_data_helper(videos)
-    else:
-        print("Invalid video index selected")
+def update_video(video_id, new_video_name, new_video_duration):
+    cursor.execute("UPDATE videos SET name = ?, time = ? WHERE id = ?", (video_id, new_video_name, new_video_duration))
+    con.commit()
 
-def delete_video(videos):
-    list_all_videos(videos)
-    index = int(input("Enter the video number to be deleted: "))
-
-    if (1 <= index <= len(videos)):
-        del videos[index-1]
-        save_data_helper(videos)
-    else:
-        print("Invalid video index selected")
+def delete_video(video_id):
+    cursor.execute("DELETE FROM videos WHERE id = ?", (video_id,))
+    con.commit()
 
 def main():
-
-    videos = load_data()
 
     while True:
         print("\nYoutube Manager \nChoose an option:")
@@ -62,26 +41,33 @@ def main():
         print("4. Delete a video")
         print("5. Exit the app")
         choice = input("Enter your choice :")
-        print(videos)
 
         match(choice):
             case '1':
-                list_all_videos(videos)
+                list_videos()
                 
             case '2':
-                add_video(videos)
+                video_name = input("Enter video name: ")
+                video_duration = input("Enter the duration: ")
+                add_video(video_name, video_duration)
                 
             case '3':
-                update_video(videos)
+                video_id = int(input("Enter the video number to update: "))
+                new_video_name = input("Enter the new video name: ")
+                new_video_duration = input("Enter the new video duration: ")
+                update_video(video_id, new_video_name, new_video_duration)
                 
             case '4':
-                delete_video(videos)
+                video_id = int(input("Enter the video number to be deleted: "))
+                delete_video(video_id)
                 
             case '5':
                 break
-            
+
             case _:
                 print("Invalid Choice")
+
+    con.close()
 
 if __name__ == "__main__":
     main()
